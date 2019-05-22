618d359bc037db10eff68b2036a7322790246d15
Toorusr <toorusr@protonmail.com>
2019-05-22 16:27:34 +0200

    Add: css /m

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
+        <link rel="stylesheet" href="/m/main.css">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
