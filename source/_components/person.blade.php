7f82145de86f4bb48c02e7afbe470c4f19383edd
Toorusr <toorusr@protonmail.com>
2019-08-13 18:10:08 +0200

    Add: person component

---
+++ source/_components/person.blade.php
@@ -0,0 +1,5 @@
+<div class="p-4 text-center md:w-1/3">
+    <img class="mx-auto h-56 rounded-full border-8 border-yellow-500" src="{{ $picture }}" alt="">
+    <h3 class="mt-4 font-extrabold text-2xl">{{ $name }}</h3>
+    <p class="font-semibold text-yellow-500">{{ $role }}</p>
+</div>
