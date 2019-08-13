bc56eb9e3d2e79736ec540d87a937b629efb806b
Toorusr <toorusr@protonmail.com>
2019-08-13 18:10:28 +0200

    Add: team-splitter component

---
+++ source/_components/team-splitter.blade.php
@@ -0,0 +1,7 @@
+<div id="team" class="flex flex-row">
+    <div class="h-32 transparent md:block md:w-2/3 z-0 flex bg-cover bg-center items-center flex-wrap justify-right  text-white" style="background-image: url('/m/p/wingtsun.training.close.jpg')">
+    </div>
+    <div class="flex items-center justify-center h-32 w-full md:w-1/3 z-20 bg-yellow-500">
+        <h2 class="text-white font-extrabold text-3xl"><span class="text-gray-900">Das</span> Team</h2>
+    </div>
+</div>
