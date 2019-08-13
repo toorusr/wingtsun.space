04d557c02f8fcb727e0d556b11dc7531db774dda
Toorusr <toorusr@protonmail.com>
2019-08-13 18:12:23 +0200

    Add: index page

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,27 +1,19 @@
 @extends('_layouts.master')
 
 @section('body')
+        @include('_partials.header')
         <div class="flex flex-wrap">
-            <div class="w-1/2">
-                <div class="flex justify-center half yellow-bg">
-                    <div class="self-center">
-                        <h1 class="font-extrabold text-5xl text-white">Willkommen</h1>
-                    </div>
-                </div>
-                <div class="flex justify-center flex-wrap half bg-white">
-                    <div class="mx-6 self-center text-center">
-                        <h2 class="mb-8 font-extrabold text-5xl black-text">Hier findest du in Zukunft die Wing<span class="yellow-text">Tsun</span> Berlin Seite.</h2>
-                        <a href="http://wt-berlin-heiligensee.de" class="black-bg hover:bg-black yellow-text font-semibold py-2 px-4 rounded">
-                          Zur jetztigen Website
-                        </a>
-                    </div>
-                </div>
+            <div class="my-auto mx-auto md:w-2/3">
+                @include('_partials.why')
             </div>
-
-            <div class="flex justify-center full w-1/2 black-bg">
-                <div class="self-center">
-                    <img src="logo.svg" alt="WingTsun Berlin-Heiligensee Logo">
-                </div>
+            <div class="flex justify-center w-full md:w-1/3">
+                @include('_partials.times')
             </div>
         </div>
+        @include('_partials.trainers')
+        <div class="my-4">
+            <h2 class="text-3xl mt-16 mb-2 text-center mx-2 font-bold">Die WingTsun Schule in deiner Nähe</h2>
+            @include('_partials.map')
+        </div>
+        @include('_components.contact')
 @endsection
