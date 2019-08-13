bea727354e16719b5944ed983dcf2d2f490178be
Toorusr <toorusr@protonmail.com>
2019-08-13 18:11:02 +0200

    Add: header banner partial

---
+++ source/_partials/header.blade.php
@@ -0,0 +1,9 @@
+<div class="border-b-8 border-yellow-500">
+    <div class="h-64 z-0 flex bg-cover bg-center items-center flex-wrap justify-right  text-white" style="background-image: url('/m/p/wingtsun.training.close.jpg')">
+            <div class="w-full absolute h-64" style="background-color:hsla(0, 0%, 0%, 0.2)"></div>
+            <div class="z-10 pl-4 md:pl-24 font-bold">
+                <h1 class="text-4xl font-extrabold">Wing<span class="text-yellow-500">Tsun</span></h1>
+                <p class="text-xl md:text-2xl">Realistisches Waffenloses <br>Selbstverteidigungssystem</p>
+            </div>
+    </div>
+</div>
