55210a7d08e6016f5d4cd9f9f37b3057626eeaeb
Toorusr <toorusr@protonmail.com>
2019-08-13 18:11:09 +0200

    Add: map partial

---
+++ source/_partials/map.blade.php
@@ -0,0 +1,17 @@
+<div class="mt-8 flex bg-cover bg-center items-center flex-wrap justify-center text-white" style="background-image: url('/m/p/wingtsun.map.png')">
+            <!-- <div class="w-full absolute h-64" style="background-color:hsla(0, 0%, 0%, 0.5)"></div> -->
+            <div class="font-semibold my-12">
+                <h3 class="text-2xl font-bold py-4">Hennigsdorfer Str. 77 <br>13503 Berlin-Heiligensee</h3>
+                <p>Anfahrt via <br>
+                   S25 Heiligensee <br>
+                   Bus 124 Kurzebracker Weg <br>
+                   Autobahnausfahrt Schulzendorf
+                </p>
+                <div class="pt-6">
+                    <a class="bg-gray-900 text-yellow-500 p-2 rounded-lg" href="#">Jetzt Anfahrt planen</a>
+                </div>
+            </div>
+</div>
+<div class="border-b-8 border-yellow-500"></div>
+
+<!-- HR -->
