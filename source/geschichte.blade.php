5eb09bbc46c6fa89cb77ca80f5c818c728e0427e
Max <hello@max.berlin>
2019-12-23 19:02:21 +0100

    Add: history page

--- source/geschichte.blade.php
+++ source/geschichte.blade.php
@@ -1,19 +1,58 @@
 @extends('_layouts.master')
 
 @section('body')
-        @include('_partials.header')
-        <div class="flex flex-wrap">
-            <div class="md:w-2/3">
-                @include('_partials.why')
-            </div>
-            <div class="md:w-1/3">
-                @include('_partials.dynceve')
-            </div>
-        </div>
-        @include('_partials.trainers')
-        <div class="my-4">
-            <h2 class="text-3xl mt-16 mb-2 text-center mx-2 font-bold">Die WingTsun Schule in deiner Nähe</h2>
-            @include('_partials.map')
-        </div>
-        @include('_components.contact')
+  <div class="m-4 md:m-8">
+    <h1 class="text-3xl text-gray-800 font-bold">Geschichte</h1>
+    <p class="text-xs mt-8 font-semibold">
+      Es begann vor ca. 250 Jahren im berühmten Shaolin Kloster. <br>
+      Die Selbstverteidigungsmeisterin Ng Mui genannt "die Kampfnonne" entwickelte aus der Kenntnis der traditionellen Kampfstile heraus eine neue, <br>
+      wirksame Selbstverteidigung, welches die schwächen der herkömlichen <br>
+      Stile nicht nur vermied, sondern für sich nutzte. Ihre erste Schülerin <br>
+      war die schöne WingTsun - um Sie zu Ehren wurde der Name des Systems beibehalten. <br>
+      WingTsun galt bis Anfang der 70. Jahre in China für die früheren Meister als Geheimtechnik und wurde nur ausgewählten Menschen zugänglich: <br>
+      insbesondere den damaligen Leibwachen hoher Persönlichkeiten (als
+      effektive Waffe) <br> um sie vor Attentaten zu schützen, damit sie sich mit
+      ihrer Kampfkunst WingTsun gegen jedes System erfolgreich verteidigen
+      konnten.
+    </p>
+    <div class="mt-16 flex flex-wrap">
+      <div class="mb-4 md:mb-0 w-full md:w-1/4">
+        <img class="mx-auto h-48 w-48 rounded-full border-8 border-yellow-500" src="/m/p/a/yip.jpg" alt="">
+      </div>
+      <div class="my-auto md:w-3/4 text-xs">
+        <h5 class="font-bold">GGM Yip Man (Great Grandmaster)</h5>
+        <p class="md:w-2/5">
+        GGM Yip Man erlaubte es dann das WingTsun ein Weltsystem wurde und
+        er nahm GM Leung Ting "Closed Door-Student" als letzten Schüler an und
+        brachte ihm alles was er konnte bei: seine langjärigen Kenntnisse,
+        Weissheit, Philosohie, die vollständige Holzpuppenform, Doppelmesser und Langstocktechniken.
+        </p>
+      </div>
+    </div>
+    <div class="mt-8 flex flex-wrap">
+      <div class="mb-4 md:mb-0 w-full md:w-1/4">
+        <img class="mx-auto h-48 w-48 rounded-full border-8 border-yellow-500" src="/m/p/a/leung.jpg" alt="">
+      </div>
+      <div class="my-auto md:w-3/4 text-xs">
+        <h5 class="font-bold">GM Leung Ting (Grandmaster)</h5>
+        <p class="md:w-2/5">
+          GM Leung Ting gründete einen Weltverband und machte WingTsun somit der ganzen Welt zugänglich.
+        </p>
+      </div>
+    </div>
+    <div class="mt-8 mb-16 flex flex-wrap">
+      <div class="mb-4 md:mb-0 w-full md:w-1/4">
+        <img class="mx-auto h-48 w-48 rounded-full border-8 border-yellow-500" src="/m/p/a/kernspecht.jpg" alt="">
+      </div>
+      <div class="my-auto md:w-3/4 text-xs">
+        <h5 class="font-bold">GM Keith R. Kernspecht (Grandmaster)</h5>
+        <p class="md:w-2/5">
+          Als ersten Europäer gelang es GM Keith R. Kernspecht das System von
+          GM Leung Ting zu erlernen und nach Europa zu bringen (dafür genießt er
+          den Dank aller WingTsun Schüler).
+        </p>
+      </div>
+    </div>
+  </div>
+
 @endsection
