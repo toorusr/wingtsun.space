6f4e3f1a9cdc89606adb99f517b7dae581fcfccf
Max <hello@max.berlin>
2019-12-23 19:02:09 +0100

    Add: privacy disclaimer

---
+++ source/datenschutz.blade.php
@@ -0,0 +1,9 @@
+@extends('_layouts.master')
+
+@section('body')
+<div class="flex flex-col m-8 my-16 md:m-64">
+  <h2 class="text-2xl font-bold">Datenschutz</h2>
+  <p class="text-xs">Ich, Mehmet Yilmaz (digital vertreten durch Maximilian Jendrall), bin der einzige Verantwortliche für die Einhaltung der GDPR-Richtlinien auf dieser Website. Ich respektiere Ihre Privatsphäre und befolge den Grundsatz der Datenvermeidung. Ich hoste diese statische Seite auf einem privaten Server und protokolliere die IP der Besucher für maximal 2 Tage. Wenn Sie mich über einen Email-Link oder das Kontaktfeld kontaktieren, erhalte ich Ihre Email-Adresse und Ihren Namen. Diese kann ich eventuell für die Beantwortung Ihrer Anfrage verwenden.  Wenn Sie mein Newsletter-Anmeldeformular abschicken, müssen Sie Ihre E-Mail-Adresse bestätigen, um meine Newsletter zu erhalten. Ich werde die Adresse nur für die Zusendung von Informationen über meine Artikel, Produkte und Dienstleistungen verwenden. Sie können sich über den Link am Ende jedes Newsletters wieder abmelden. Ich benutze das selbst gehostete Tool Fathom, um anonyme Seitenbesuche und Referrer über ein 30-minütiges Session-Cookie zu sammeln.
+  </p>
+</div>
+@endsection
