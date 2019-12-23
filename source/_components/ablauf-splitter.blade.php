00fba1fc0810c34a0f8cddb7389190b6c7f2f0a0
Max <hello@max.berlin>
2019-12-23 18:59:13 +0100

    Add: ablauf splitter component

---
+++ source/_components/ablauf-splitter.blade.php
@@ -0,0 +1,7 @@
+<div id="ablauf" class="flex flex-row">
+    <div class="flex items-center justify-center h-32 w-full md:w-1/3 z-20 bg-yellow-500">
+        <h2 class="text-white font-extrabold text-3xl"><span class="text-gray-900">Der</span> Ablauf</h2>
+    </div>
+    <div class="h-32 transparent md:block md:w-2/3 z-0 flex bg-cover bg-center items-center flex-wrap justify-right  text-white" style="background-image: url('/m/p/wingtsun.training.close.jpg')">
+    </div>
+</div>
