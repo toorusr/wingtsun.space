6996679f7c5cc16ae8ad7cf94c87dda534c33a42
Max <hello@max.berlin>
2019-12-23 19:02:27 +0100

    Add: imprint

---
+++ source/impressum.blade.php
@@ -0,0 +1,21 @@
+@extends('_layouts.master')
+
+@section('body')
+<div class="flex flex-col m-8 my-16 md:m-64">
+  <h2 class="text-2xl font-bold">Verantwortliche Person nach §5 TMG:</h2>
+  <p>WingTsun-Kampfkunstschule Berlin-Heiligensee</p>
+  <p>Sihing <span class="font-bold">Mehmet Yilmaz</span> 2.TG</p>
+  <p>Hennigsdorfer Str. 77</p>
+  <p>13503 Berlin-Heiligensee</p>
+  <p>Telefon: 030 - 233 580 47</p>
+  <p>Mobil: 0176 7918 1414 </p>
+  <h3 class="text-xl font-bold mt-8">Haftungshinweise</h3>
+  <p class="text-xs mt-4">
+  Diese Seite enthält Links zu externen Webseiten Dritter, auf deren Inhalte ich keinen Einfluss habe. Deshalb übernehme ich keine Gewähr für die Inhalte dieser Seiten. Für diese Inhalte sind stets die jeweiligen Anbieter / Betreiber der Seiten verantwortlich. Bei bekannt werden von Rechtsverletzungen werden derartige Links umgehend entfernt.
+  Soweit auf dieser Seite personenbezogene Daten (beispielsweise Name, Anschrift eMail-Adressen) oder Fotos erhoben werden, erfolgt dies mit der schriftlichen Einverständnis der Betroffenen.
+  Ich weise darauf hin, dass die Datenübertragung im Internet Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. Ich übernehme keine Verantwortung für Schäden an Ihrem System.
+  Das Kopieren von Film- und Fotomaterial von dieser Seite ohne Einverständnis meinerseits ist ausdrücklich untersagt!
+  Bitte nehmen Sie dies zur Kenntnis.
+  </p>
+</div>
+@endsection
