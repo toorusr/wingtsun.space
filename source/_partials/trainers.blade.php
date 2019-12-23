9641625bfa03169de8056bb965494cafd1920250
Max <hello@max.berlin>
2019-12-23 19:01:26 +0100

    Cut: trainer images

--- source/_partials/trainers.blade.php
+++ source/_partials/trainers.blade.php
@@ -1,14 +1,14 @@
 <div class="my-8 bg-yellow-500 p-8 md:p-16">
     <h2 class="text-3xl md:text-4xl font-semibold">Trainiert werden <br>von zertifizierten WingTsun Trainern</h2>
     <div class="flex flex-wrap mt-16 justify-center">
-        <div class="mx-8 w-auto font-bold text-gray-900">
-            <img class="bg-white  rounded-lg h-48" src="/m/p/a/max.jpg" alt="">
+        <div class="mx-8 w-auto mb-8 md:mb-0 font-bold text-gray-900">
+            <img class="bg-white  rounded-lg h-48 w-48" src="/m/p/a/mehmet.jpg" alt="Portrait Mehment Yilmaz">
             <h3 class="pt-2 pb-1 text-2xl">Mehmet Yilmaz</h3>
             <p>Schulleiter</p>
         </div>
         <div class="mx-8 w-auto font-bold text-gray-900">
-            <img class="bg-white  rounded-lg h-48" src="/m/p/a/max.jpg" alt="">
-            <h3 class="pt-2 pb-1 text-2xl">Manfred</h3>
+            <img class="bg-white  rounded-lg h-48 w-48" src="/m/p/a/manfred.jpg" alt="Portrait Manfred Müller">
+            <h3 class="pt-2 pb-1 text-2xl">Manfred Müller</h3>
             <p>Co-Trainer</p>
         </div>
     </div>
