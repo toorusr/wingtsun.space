43fc10ca45ecb8273e60c8c62761cff5b999b419
Max <hello@max.berlin>
2019-12-23 19:00:28 +0100

    Add: lastname to person

--- source/_components/person.blade.php
+++ source/_components/person.blade.php
@@ -1,5 +1,5 @@
 <div class="p-4 text-center md:w-1/3">
     <img class="mx-auto h-56 rounded-full border-8 border-yellow-500" src="{{ $picture }}" alt="">
-    <h3 class="mt-4 font-extrabold text-2xl">{{ $name }}</h3>
+    <h3 class="mt-4 font-extrabold text-2xl">{{ $name }} <span class="text-yellow-500">{{ $lastname }}</span></h3>
     <p class="font-semibold text-yellow-500">{{ $role }}</p>
 </div>
