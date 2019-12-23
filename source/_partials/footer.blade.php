c6e894638303c2474e1674af22d1c9d9eeab1f56
Max <hello@max.berlin>
2019-12-23 19:00:42 +0100

    Fix: footer credits

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-black flex flex-col justify-center items-center font-bold text-xs p-10">
     <p class="py-1 text-yellow-600">© 2003-2019 WingTsun Berlin-Heiligensee</p>
-    <p class="text-yellow-800">Webentwicklung realisiert von <a class="text-yellow-700" href="https://max.berlin/?ref=wingtsun.space">max.berlin</a></p>
+    <p class="text-yellow-800">Webentwicklung ehrenamtlich realisiert von <a class="text-yellow-700" href="https://max.berlin/?ref=wingtsun.space">max.berlin</a></p>
     <div class="py-1">
         <a class="pr-1 text-yellow-600" href="/impressum">IMPRESSUM</a>
         <a class="text-yellow-600" href="/datenschutz">DATENSCHUTZ</a>
