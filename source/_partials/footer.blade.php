6da083b31d38886df24c12eac9e56287cf732f37
Max <hello@max.berlin>
2020-01-02 16:49:20 +0100

    Add: dynamic copyright year

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-black flex flex-col justify-center items-center font-bold text-xs p-10">
-    <p class="py-1 text-yellow-600">© 2003-2019 WingTsun Berlin-Heiligensee</p>
+    <p class="py-1 text-yellow-600">© 2003-<script> var date = new Date(); var yy = date.getYear(); var year = (yy < 1000) ? yy + 1900 : yy; document.write(year); </script> WingTsun Berlin-Heiligensee</p>
     <p class="text-yellow-800">Webentwicklung ehrenamtlich realisiert von <a class="text-yellow-700" href="https://max.berlin/?ref=wingtsun.space">max.berlin</a></p>
     <div class="py-1">
         <a class="pr-1 text-yellow-600" href="/impressum">IMPRESSUM</a>
